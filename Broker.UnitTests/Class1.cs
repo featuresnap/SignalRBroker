0ed49d73726266f32137938b54e3a6286a7d10cf
Patrick Kelly <patrick.kelly@sherwin.com>
2017-06-07 06:24:20 -0400

    Exploring Dependency Injection for SignalR

---
+++ Broker.UnitTests/Class1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Practices.Unity;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Broker.UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Container_ResolveAll_Handles_Type_Never_Registered()
+        {
+            var container = new UnityContainer();
+            var registrations = container.ResolveAll(typeof(IDisposable));
+            CollectionAssert.IsEmpty(registrations);
+        }
+        [Test]
+        public void Container_Resolve_Throws_With_Type_Never_Registered()
+        {
+            var container = new UnityContainer();
+            Assert.Throws<ResolutionFailedException>(() => container.Resolve(typeof(IDisposable)));
+        }
+    }
+}
---
+++ Broker/App_Start/UnityDependencyResolver.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Broker.App_Start
+{
+    public class UnityDependencyResolver : DefaultDependencyResolver
+    {
+        private IUnityContainer _container;
+
+        public UnityDependencyResolver(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public override object GetService(Type serviceType)
+        {
+            if (_container.IsRegistered(serviceType))
+            {
+                return _container.Resolve(serviceType);
+            }
+
+            return base.GetService(serviceType);
+        }
+
+        public override IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
